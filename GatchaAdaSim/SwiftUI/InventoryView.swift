c52cb6d46d59f2d42da4a8dba3dc3e9ccebb9859
alesdematteo01 <62564507+alesdematteo01@users.noreply.github.com>
2022-03-31 14:41:37 +0200

    Inventory added

---
+++ GatchaAdaSim/SwiftUI/InventoryView.swift
@@ -0,0 +1,20 @@
+//
+//  InventoryView.swift
+//  GatchaAdaSim
+//
+//  Created by Alessandra De Matteo on 31/03/22.
+//
+
+import SwiftUI
+
+struct InventoryView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct InventoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        InventoryView()
+    }
+}
