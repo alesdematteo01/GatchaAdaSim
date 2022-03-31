047fcce7db646d23c07789792f3d536ab3eca8a1
alesdematteo01 <62564507+alesdematteo01@users.noreply.github.com>
2022-03-31 14:31:54 +0200

    Added PulledViewController

---
+++ GatchaAdaSim/UIKit/PullViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PullViewController.swift
+//  GatchaAdaSim
+//
+//  Created by Alessandra De Matteo on 31/03/22.
+//
+
+import Foundation
+import UIKit
+
+class PullViewController: UIViewController {
+    
+}
