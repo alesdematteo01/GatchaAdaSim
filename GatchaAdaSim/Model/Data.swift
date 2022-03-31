80a500b2ee7d2f66fb0b719e3544c7f9b5b32c73
alesdematteo01 <62564507+alesdematteo01@users.noreply.github.com>
2022-03-31 14:23:51 +0200

    Added Data

---
+++ GatchaAdaSim/Model/Data.swift
@@ -0,0 +1,48 @@
+//
+//  Data.swift
+//  GatchaAdaSim
+//
+//  Created by Alessandra De Matteo on 31/03/22.
+//
+
+import Foundation
+import UIKit
+
+var characters: [Character] = [
+    Character(
+        name: "Giovanni",
+        surname: "Monaco",
+        rarity: "5",
+        rarityColor: UIColor(named: "Yellow")
+    ),
+    Character(
+        name: "Domenico",
+        surname: "Tancredi",
+        rarity: "5",
+        rarityColor: UIColor(named: "Yellow")
+    ),
+    Character(
+        name: "Mara",
+        surname: "Rossi",
+        rarity: "5",
+        rarityColor: UIColor(named: "Yellow")
+    ),
+    Character(
+        name: "Roberta",
+        surname: "Albanese",
+        rarity: "5",
+        rarityColor: UIColor(named: "Yellow")
+    ),
+    Character(
+        name: "Moritz",
+        surname: "Recke",
+        rarity: "5",
+        rarityColor: UIColor(named: "Yellow")
+    ),
+    Character(
+        name: "Tiago",
+        surname: "Pereira",
+        rarity: "5",
+        rarityColor: UIColor(named: "Yellow")
+    )
+]
---
+++ GatchaAdaSim/SwiftUI/PulledCharacter.swift
@@ -0,0 +1,12 @@
+//
+//  PulledCharacter.swift
+//  GatchaAdaSim
+//
+//  Created by Alessandra De Matteo on 31/03/22.
+//
+
+import SwiftUI
+
+struct PulledCharacter {
+    
+}
